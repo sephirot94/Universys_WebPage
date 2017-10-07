f40a28e7d2f68dff0292e5775219cdeba1f619e9
Jinkus <ivan.jinkus@accenture.com>
2017-10-07 13:06:10 -0300

    terminado el metodo redireccionador

--- js/utility.js
+++ js/utility.js
@@ -1,8 +1,10 @@
+//Esta funcion recorre el mapa de codigos que devuelve la api y retorna el valor del codigo devuelto en el mapa, 
+//que contiene una pagina a la que el sitio redireccionara. Luego, redirecciona a esa pagina.
 function redirectorSearch(json, mapa) {
     var codigo = json.error-code;
     for(var key in mapa) {
-        if()codigo==key) {
-            window.location.href = //key[value] devolver value referido de la key ;
+        if(codigo==key) {
+            window.location.href = '../html/' + key[value];
         }
     }
 }
