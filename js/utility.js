dc4bff91b619fc25419cc95f33294a340e81f844
Jinkus <ivan.jinkus@accenture.com>
2017-10-06 21:36:05 -0300

    creado el script para redireccionar

---
+++ js/utility.js
@@ -0,0 +1,8 @@
+function redirectorSearch(json, mapa) {
+    var codigo = json.error-code;
+    for(var key in mapa) {
+        if()codigo==key) {
+            window.location.href = //key[value] devolver value referido de la key ;
+        }
+    }
+}
