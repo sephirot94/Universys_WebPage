ed546312d42e3938ad31b7fa9e9eaf9442077fa0
Jinkus <ivan.jinkus@accenture.com>
2017-10-22 12:51:07 -0300

    Unit test Register Class

--- js/register.js
+++ js/register.js
@@ -50,15 +50,15 @@ function RegisterControllerLocal(RegisterControllerBase)
         if (flag=="fichadas") {
             return '{ "api-version" : "1.0", "error-code" : "200",\ 
             "usuario": "{\
-                “catedra” : “”,\
-                “carrera” : “”,\
-                “materia” : “”,\
-                “horario” : “”,\
+                “catedra” : “Didier”,\
+                “carrera” : “Programacion”,\
+                “materia” : “Proyecto”,\
+                “horario” : “19:00hs”,\
                 “tipoDeClase” : “teorico”,\
-                “claveDeClase” : "",\
+                “claveDeClase” : "30",\
                 “alumnos” : [\
-                            {“nombre” : ””,\
-                            “presente” : “”}\
+                            {“nombre” : ”ivan”,\
+                            “presente” : true}\
                     ]\
             }"\
         }';
@@ -67,10 +67,10 @@ function RegisterControllerLocal(RegisterControllerBase)
         if (flag=="catedras") {
             return '{ "api-version" : "1.0", "error-code" : "200",\ 
             "catedra": "{\
-                “catedra” : “”,\
-                “nombre” : “”,\
-                “titularDeCatedra” :””,\
-                “ofertaHoraria” : “”,\
+                “catedra” : “Didier”,\
+                “nombre” : “Proyecto”,\
+                “titularDeCatedra” :”Didier”,\
+                “ofertaHoraria” : “Lun-Vie, 19:00 a 23:00”,\
             }"\
         }';
         }
@@ -78,9 +78,9 @@ function RegisterControllerLocal(RegisterControllerBase)
         if (flag=="materias") {
             return '{ "api-version" : "1.0", "error-code" : "200",\ 
             "materia": "{\
-                “catedra” : “”,\
-                “carrera” : "",\
-                “materia” : “”,\
+                “catedra” : “Didier”,\
+                “carrera” : "Programacion",\
+                “materia” : “Proyecto”,\
             }"\
         }';
         }
@@ -141,12 +141,28 @@ function testRegister() {
 
     var json = parsejsonstring(rc.register("usuario"));
 
+    //Chequeo JSON Usuario
     if(json.usuario.nombre != "Gaston" || json.usuario.apellido != "Bodeman" || json.usuario.fnac != "20/09/1994" || json.usuario.genero != "masculino" || json.usuario.domicilio != "blanco encalada 4892" || json.usuario.telefono != "45228786" || json.usuario.matricula != "03140" || json.usuario.mail != "pepito@gmail.com") {
-        return "testLogin has ERRORS: loginLogic not returning the right value. Register: Usuario";
+        return "testRegister has ERRORS: RegisterController not returning the right value RegisterController::Controller:: Usuario";
     }
 
+    //Chequeo JSON Notas
     if(json.notas.catedra != "Didier" || json.notas.carrera != "Programacion" || json.notas.materia != "Proyecto" || json.notas.horario != "19:00hs" || json.notas.tipoDeClase != "teorico" || json.notas.claveDeClase != "30" || json.notas.alumno != "ivan" || json.notas.nota != "4") {
-        return "testLogin has ERRORS: loginLogic not returning the right value. Register: Notas";
+        return "testRegister has ERRORS: RegisterController not returning the right value. RegisterController::: Notas";
+    }
+
+    //Chequeo JSON Fichadas
+    if(json.fichadas.catedra != "Didier" || json.fichadas.carrera != "Programacion" || json.fichadas.materia != "Proyecto" || json.fichadas.horario != "19:00hs" || json.fichadas.tipoDeClase != "teorico" || json.fichadas.claveDeClase != "30" || json.fichadas.alumnos[0].nombre != "ivan" || !json.fichadas.alumnos[0].presente) {
+        return "testRegister has ERRORS: RegisterController not returning the right value. RegisterController::: Fichadas";
+    }
+
+    //Chequeo JSON Catedra
+    if(json.catedra.catedra != "Didier" || json.catedra.nombre != "Proyecto" || json.catedra.titularDeCatedra != "Didier" || json.catedra.ofertaHoraria != "Lun-Vie, 19:00 a 23:00" ) {
+        return "testRegister has ERRORS: RegisterController not returning the right value. RegisterController::: catedra";
+    }
+
+    //Chequeo JSON Materia
+    if(json.materia.catedra != "Didier" || json.materia.materia != "Proyecto" || json.materia.carrera != "Programacion") {
+        return "testRegister has ERRORS: RegisterController not returning the right value. RegisterController::: materia";
     }
 }
-
